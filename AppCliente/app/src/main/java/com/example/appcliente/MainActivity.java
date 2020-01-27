d81a37c6263eeaebdf6081fb4abaa1acdc0375d5
Arlyn Cotrado <alejandra321.any@gmail.com>
2020-01-26 22:25:30 -0500

    menu principal

---
+++ AppCliente/app/src/main/java/com/example/appcliente/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.appcliente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(this, Login.class));
+    }
+
+    public void registro(View view) {
+        startActivity(new Intent(this, Registro.class));
+    }
+}
