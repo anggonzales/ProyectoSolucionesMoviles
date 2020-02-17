f2c3b2a2a898b544a09d4fa12df846aa2afaf4f4
Arlyn Cotrado <alejandra321.any@gmail.com>
2020-02-17 01:23:33 -0500

    login clientes con Firebase Authentication

--- AppCliente/app/src/main/java/com/example/appcliente/Login.java
+++ AppCliente/app/src/main/java/com/example/appcliente/Login.java
@@ -1,20 +1,65 @@
 package com.example.appcliente;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    private EditText correo,pass;
+    private Button boton;
+
+    private String email="";
+    private String password="";
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        correo=(EditText) findViewById(R.id.edtusuario);
+        pass=(EditText) findViewById(R.id.edtpass);
+        boton=(Button) findViewById(R.id.btnlogin);
+        mAuth = FirebaseAuth.getInstance();
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email=correo.getText().toString();
+                password=pass.getText().toString();
+
+                if (!email.isEmpty()&&!password.isEmpty()){
+                    loginuser();
+                }else{
+                    Toast.makeText(Login.this,"Debe completar los campos",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    public void inicio(View view) {
-        startActivity(new Intent(this, Inicio.class));
+
+    private void loginuser(){
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    startActivity(new Intent(Login.this, Inicio.class));
+                    finish();
+                }else{
+                    Toast.makeText(Login.this,"No se pudo iniciar sesión. Compruebe los datos",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- AppCliente/app/src/main/java/com/example/appcliente/MainActivity.java
+++ AppCliente/app/src/main/java/com/example/appcliente/MainActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void registro(View view) {
         startActivity(new Intent(this, Registro.class));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        if (mAuth.getCurrentUser()!=null){
+
+      //  }
     }
 }
